435e71be47e8603eafac33cc4dd8b8a460b6e517
josno <jnsenoren@gmail.com>
2020-10-03 00:57:52 +0400

    Update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const toggleMenu = () => {
 	document.querySelector(".top").classList.toggle("top-active");
 	document.querySelector(".middle").classList.toggle("middle-active");
 	document.querySelector(".bottom").classList.toggle("bottom-active");
+	document.querySelector(".mobile-nav-menu").classList.toggle("active");
 };
 
 navBtn.addEventListener("click", () => {
